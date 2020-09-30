9e7ad32507c1e6019aa1b358917084adbdedca08
Eric Watyeks <ewatyeks@gmail.com>
2020-09-30 17:01:08 +0300

    Auto Commit

--- autocommit.bat
+++ autocommit.bat
@@ -1,7 +1,6 @@
 @echo off
 :TOP
 cd "C:\Users\CosekeSysDev\ericytex.github.io"
-For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
 git stage .
 git commit -m "Auto Commit"
 git push origin master
