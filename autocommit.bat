07d15108b4660be48d532cd143236543be918284
Eric Watyeks <ewatyeks@gmail.com>
2020-09-30 17:07:35 +0300

    Auto commit 30-Sep-20 17:07:35.66

--- autocommit.bat
+++ autocommit.bat
@@ -2,6 +2,6 @@
 :TOP
 cd "C:\Users\CosekeSysDev\ericytex.github.io"
 git stage .
-git commit -m " Auto commit %date% %Time%"
+git commit -m " Auto commit %date% %time%"
 git push origin master
 goto :TOP
